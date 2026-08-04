4474c0c1f9ee015c4c4452e49c5d4af28e5b261c
fahad mohammed <fahadmoha2006@gmail.com>
2026-08-05 02:22:41 +0400

    Implement CRUD operations for exercises

--- operations.py
+++ operations.py
@@ -1,18 +1,31 @@
 from db_connection import connection, cursor
 
-def get_exercises():
-    query = "SELECT * FROM exercises"
-    cursor.execute(query)
-    exercises = cursor.fetchall()
-    return exercises
-
-def get_muscle_groups():
-    query = "SELECT * FROM musclegroups"
-    cursor.execute(query)
-    muscle_groups = cursor.fetchall()
-    return muscle_groups
-
 def add_exercise(exercise_name, equipment, difficulty):
     query = "INSERT INTO exercises (exercise_name, equipment, difficulty) VALUES (%s, %s, %s)"
     cursor.execute(query, (exercise_name, equipment, difficulty))
     connection.commit()
+    return cursor.lastrowid
+
+def get_exercises():
+    query = "SELECT * FROM exercises"
+    cursor.execute(query)
+    return cursor.fetchall()
+     
+
+def get_exercise_by_id(exercise_id):
+    query = "SELECT * FROM exercises WHERE exercise_id = %s"
+    cursor.execute(query, (exercise_id,))
+    return cursor.fetchone()
+     
+
+def update_exercise(exercise_id, exercise_name, equipment, difficulty):
+    query = "UPDATE exercises SET exercise_name = %s, equipment = %s, difficulty = %s WHERE exercise_id = %s"
+    cursor.execute(query, (exercise_name, equipment, difficulty, exercise_id))
+    connection.commit()
+    return cursor.rowcount
+
+def delete_exercise(exercise_id):
+    query = "DELETE FROM exercises WHERE exercise_id = %s"
+    cursor.execute(query, (exercise_id,))
+    connection.commit()
+    return cursor.rowcount
